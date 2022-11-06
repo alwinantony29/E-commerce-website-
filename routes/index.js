5afc3f39a49d6818d673283eebcae79a84ae9723
alwin antony <alwinanto5885@gmail.com>
2022-11-06 11:43:03 -0800

    changed name,added header,cards,images,description.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products=[
+    {
+      name:"blackforest",
+      catogery:"cake",
+      description:"nice chocolate cake",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcYG6vZfOMuWeC8Sfg73cOLbqDdZ-FmDARGQ&usqp=CAU"
+    },
+    {
+
+      name:"whiteforest",
+      catogery:"cake",
+      description:"nice cake",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRijAXx0uX6pjAseTvf2O-RGOA_-o8G6sR6aA&usqp=CAU"
+
+    },
+    {
+      name:"blackforest",
+      catogery:"cake",
+      description:"nice chocolate cake",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcYG6vZfOMuWeC8Sfg73cOLbqDdZ-FmDARGQ&usqp=CAU"
+    },
+    {
+
+      name:"whiteforest",
+      catogery:"cake",
+      description:"nice cake",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRijAXx0uX6pjAseTvf2O-RGOA_-o8G6sR6aA&usqp=CAU"
+
+    },
+  ]
+  res.render('index', {products});
 });
 
 module.exports = router;
