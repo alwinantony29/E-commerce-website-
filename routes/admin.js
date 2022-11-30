16134afb81d703e166d62ebc3af5c97d4e95cef9
alwin antony <alwinanto5885@gmail.com>
2022-11-30 07:52:08 -0800

    database product retrieved

--- routes/admin.js
+++ routes/admin.js
@@ -5,48 +5,31 @@ var productHelper=require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
-  let products=[
-    {
-      name:"blackforest",
-      category:"cake",
-      description:"nice chocolate cake",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcYG6vZfOMuWeC8Sfg73cOLbqDdZ-FmDARGQ&usqp=CAU"
-    },
-    {
-
-      name:"whiteforest",
-      category:"cake",
-      description:"nice cake",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRijAXx0uX6pjAseTvf2O-RGOA_-o8G6sR6aA&usqp=CAU"
-
-    },
-    {
-      name:"blackforest",
-      category:"cake",
-      description:"nice chocolate cake",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcYG6vZfOMuWeC8Sfg73cOLbqDdZ-FmDARGQ&usqp=CAU"
-    },
-    {
-
-      name:"whiteforest",
-      category:"cake",
-      description:"nice cake",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRijAXx0uX6pjAseTvf2O-RGOA_-o8G6sR6aA&usqp=CAU"
-
-    },
-  ]
-
+productHelper.getAllProducts().then((products)=>{
+  console.log(products)
+  console.log('bye');
   res.render('admin/view-products',{admin:true,products})
+
+})
 });
 router.get('/add-product',function(req,res){
   res.render('admin/add-product')
 })
 router.post('/add-product',(req,res)=>{
-  console.log(req.body);
-  console.log(req.files.Image);
-  productHelpers.addProduct(req.body,(result)=>{
-    res.render("admin/add-product")
+  
+  
+  productHelpers.addProduct(req.body,(id)=>{
+    console.log(id);
+    let image=req.files.Image
+    image.mv('public/product-images/'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-product") 
+      }
+      else{
+        console.log(err);
+      }
+    })
+    
   })
 })
 
