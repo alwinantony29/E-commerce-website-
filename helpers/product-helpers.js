2ba8c8de95b1cc247fe67638187ac43aed641188
alwin antony <alwinanto5885@gmail.com>
2022-12-03 13:49:08 +0530

    feat : edit button (for updating product details & image)

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 
 var db=require('../config/connection')
-var collection=require('../config/collections')
+var collection=require('../config/collections');
+const { response } = require('express');
 var objectId=require('mongodb').ObjectId
 module.exports={
     addProduct:(product,callback)=>{
@@ -19,11 +20,33 @@ module.exports={
         })
 
     },
-     deleteProduct:(prodId)=>{
+     deleteProduct:(proId)=>{
         return new Promise((resolve,reject)=>{
-            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(prodId)}).then((response)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(proId)}).then((response)=>{
                 console.log(response);
                 resolve(response);
+            })
+        })
+     },
+     getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product);
+            })
+        })
+     },
+     updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Price:proDetails.Price,
+                    Category:proDetails.Category
+                }
+            }).then((response)=>{
+                resolve()
             })
         })
      }
