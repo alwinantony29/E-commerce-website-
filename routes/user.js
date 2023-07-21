63283ec8310ce7cede7f4f4e43815b05e7878f1f
alwin antony <alwinanto5885@gmail.com>
2023-07-21 21:54:56 +0530

    clean up

--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,6 @@ const verifyLogin = (req, res, next) => {
 router.get('/', async function (req, res, next) {
   try {
     let user = req.session.user;
-    // console.log("user from cookies: ",req.cookies.user);
     let cartCount;
     if (user) {
       cartCount = await cartHelpers.getCartCount(req.session.user._id);
@@ -51,10 +50,8 @@ router.post('/login', (req, res, next) => {
   userHelpers.doLogin(req.body)
     .then((response) => {
       if (response.status) {
-        console.log("login response",response.user);
         req.session.user = response.user;
         req.session.user.loggedIn = true;
-       // res.cookie('user', "alwin", { maxAge: 24 * 60 * 60 * 1000 });
         res.redirect('/');
       } else {
         req.session.userloginErr = 'Invalid username or Password';
@@ -80,10 +77,8 @@ router.post('/signup', (req, res, next) => {
   userHelpers
     .doSignup(req.body)
     .then((response) => {
-      console.log(response);
       req.session.user = response;
       req.session.user.loggedIn = true;
-      // res.cookie('user', response._id, { maxAge: 24 * 60 * 60 * 1000 });
       res.redirect('/');
     })
     .catch((error) => {
@@ -92,7 +87,7 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.get('/logout', (req, res) => {
-  // res.cookie('user', '', { expires: new Date(0) });
+  req.session.user = false
   req.session.userLoggedIn = false
   res.redirect('/')
 })
